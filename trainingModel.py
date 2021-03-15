17a5e927045219d1b699ebf93f1068aac24f7412
DoNgocHuu <ngochuu.bts@gmail.com>
2021-03-15 22:00:58 +0700

    fix bug - draw img

--- trainingModel.py
+++ trainingModel.py
@@ -145,7 +145,7 @@ model.add(Dense(7, activation='softmax'))
 # model.make_model()
 # model.load_weights()
 callback = tf.keras.callbacks.EarlyStopping(
-    monitor='val_accuracy', patience=5,
+    monitor='val_loss', min_delta=0, patience=5,
     mode='auto', restore_best_weights=True
 )
 model.compile(loss=categorical_crossentropy,optimizer=Adam(),metrics=['accuracy'])
@@ -167,6 +167,7 @@ acc = history_dict['accuracy']
 val_acc = history_dict['val_accuracy']
 loss = history_dict['loss']
 val_loss = history_dict['val_loss']
+epochs = range(1, len(acc) + 1)
 
 # "bo" is for "blue dot"
 plt.plot(epochs, loss, 'bo', label='Training loss')
@@ -176,7 +177,6 @@ plt.title('Training and validation loss')
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
 plt.legend()
-
 plt.savefig('trainingLoss.png')
 
 plt.plot(epochs, acc, 'bo', label='Training acc')
@@ -185,7 +185,6 @@ plt.title('Training and validation accuracy')
 plt.xlabel('Epochs')
 plt.ylabel('Accuracy')
 plt.legend(loc='lower right')
-
 plt.savefig('traningAcc.png')
 
 #saving model
