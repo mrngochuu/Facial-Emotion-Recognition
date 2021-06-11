195ef4283b99b2c0ccd69a596c21c2ccea2b71e8
DoNgocHuu <ngochuu.bts@gmail.com>
2021-06-11 14:09:02 +0700

    [HuuDN] add args -video -save -fps

---
+++ videoOffline-applyThreading.py
@@ -0,0 +1,97 @@
+import os
+import cv2
+import time
+import numpy as np
+import argparse
+import imutils
+import tensorflow as tf
+from tensorflow.keras.models import model_from_json
+from tensorflow.keras.preprocessing import image
+from imutils.video import FileVideoStream
+from imutils.video import FPS
+import FileVideoStreamCustom
+
+
+#load model
+model = model_from_json(open("fer.json","r").read())
+#load weights
+model.load_weights('fer.h5')
+
+face_haar_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# cap=cv2.VideoCapture('video_offline/test.mp4')
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", required=True, help="path to input video file")
+ap.add_argument("-q", "--queue", type=int, default=128, help="queue to put frames")
+ap.add_argument("--fps", type=int, default=0, help="fps to detect video")
+args = vars(ap.parse_args())
+
+# start the file video stream thread and allow the buffer to
+# start to fill
+print("[INFO] starting video file thread...")
+fvs = FileVideoStreamCustom(args["video"], args['queue'], args['fps']).start()
+time.sleep(1.0)
+# start the FPS timer
+fps = FPS().start()
+
+# used to record emotions
+i = 0
+result = [0, 0, 0, 0, 0, 0, 0]
+
+while fvs.more():
+    frame = fvs.read()
+    # frame = imutils.resize(frame, width=450)
+    
+    # ret,test_img=cap.read()# captures frame and returns boolean value and captured image
+    textEmotion = ''
+    # if not frame:
+    #     continue
+    gray_img= cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
+
+    for (x,y,w,h) in faces_detected:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),thickness=7)
+        roi_gray=gray_img[y:y+w,x:x+h]#cropping region of interest i.e. face area from  image
+        roi_gray=cv2.resize(roi_gray,(48,48))
+        img_pixels = image.img_to_array(roi_gray)
+        img_pixels = np.expand_dims(img_pixels, axis = 0)
+        img_pixels /= 255
+
+        predictions = model.predict(img_pixels)
+
+        #find max indexed array
+        max_index = np.argmax(predictions[0])
+        if max_index >= 0:
+            result[max_index] += 1
+
+        emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
+        predicted_emotion = emotions[max_index]
+
+        cv2.putText(frame, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+        textEmotion = predicted_emotion
+        
+   
+    resized_img = cv2.resize(frame, (1000, 700))
+    cv2.imshow('Facial emotion analysis ',resized_img)
+    print('--------------------------------')
+    print(str(i) + ': ' + textEmotion)
+    print(*result, sep = ", ")
+    print('--------------------------------')
+    i += 1
+
+    if cv2.waitKey(10) == ord('q'):#wait until 'q' key is pressed
+        break
+    fps.update()
+    # stop the timer and display FPS information
+    fps.stop()
+    print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+cv2.destroyAllWindows()
+fvs.stop()
+
+# cap.release()
+# cv2.destroyAllWindows
--- videoOffline.py
+++ videoOffline.py
@@ -2,10 +2,17 @@ import os
 import cv2
 import time
 import numpy as np
+import argparse
 
 import tensorflow as tf
 from tensorflow.keras.models import model_from_json
 from tensorflow.keras.preprocessing import image
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", required=True, help="path to input video file")
+ap.add_argument("-s", "--save", required=True, help="path to save detected video")
+ap.add_argument("--fps", type=int, default=0, help="fps to detect video")
+args = vars(ap.parse_args())
 
 #load model
 model = model_from_json(open("fer.json","r").read())
@@ -14,54 +21,101 @@ model.load_weights('fer.h5')
 
 face_haar_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-cap=cv2.VideoCapture('video_offline/video_3.mp4')
+
+
+# input video
+cap = cv2.VideoCapture(args["video"])
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fpsCap = cap.get(cv2.CAP_PROP_FPS)
+
+# arg fps
+fpsReduce = args["fps"]
+
+# flg to check apply fps
+fpsFlg = True
+if fpsReduce <= 0 or fpsReduce >= fpsCap:
+    fpsReduce = fpsCap
+    fpsFlg = False
+
+# output video
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter(args["save"], fourcc, fpsReduce, (width,height))
 
 # used to record emotions
 i = 0
 result = [0, 0, 0, 0, 0, 0, 0]
 
-while True:
-    ret,test_img=cap.read()# captures frame and returns boolean value and captured image
-    textEmotion = ''
-    if not ret:
-        continue
-    gray_img= cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
+# flg to get detect frame
+flg = True
 
-    faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
+# count total detect frame
+count = 0
 
-    for (x,y,w,h) in faces_detected:
-        cv2.rectangle(test_img,(x,y),(x+w,y+h),(255,0,0),thickness=7)
-        roi_gray=gray_img[y:y+w,x:x+h]#cropping region of interest i.e. face area from  image
-        roi_gray=cv2.resize(roi_gray,(48,48))
-        img_pixels = image.img_to_array(roi_gray)
-        img_pixels = np.expand_dims(img_pixels, axis = 0)
-        img_pixels /= 255
+while cap.isOpened():
+    # captures frame and returns boolean value and captured image
+    ret,test_img=cap.read()
+    # get frame number
+    currentFrame = cap.get(cv2.CAP_PROP_POS_FRAMES)
 
-        predictions = model.predict(img_pixels)
+    if fpsFlg:
+        if flg:
+            firstFrame = currentFrame
+            lastFrame = currentFrame + fpsCap
 
-        #find max indexed array
-        max_index = np.argmax(predictions[0])
-        if max_index >= 0:
-            result[max_index] += 1
+        if currentFrame == lastFrame:
+            flg = True
 
-        emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
-        predicted_emotion = emotions[max_index]
-
-        cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
-        textEmotion = predicted_emotion
+    if (fpsFlg == False) or (currentFrame >= firstFrame and currentFrame <= firstFrame + fpsReduce):
+        count = count + 1
+        flg = False
         
-   
-    resized_img = cv2.resize(test_img, (1000, 700))
-    cv2.imshow('Facial emotion analysis ',resized_img)
+        textEmotion = ''
+        if not ret:
+            continue
+        gray_img= cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
+
+        faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
+
+        for (x,y,w,h) in faces_detected:
+            cv2.rectangle(test_img,(x,y),(x+w,y+h),(255,0,0),thickness=7)
+            roi_gray=gray_img[y:y+w,x:x+h] #cropping region of interest i.e. face area from  image
+            roi_gray=cv2.resize(roi_gray,(48,48))
+            img_pixels = image.img_to_array(roi_gray)
+            img_pixels = np.expand_dims(img_pixels, axis = 0)
+            img_pixels /= 255
+
+            predictions = model.predict(img_pixels)
+
+            #find max indexed array
+            max_index = np.argmax(predictions[0])
+            if max_index >= 0:
+                result[max_index] += 1
+
+            emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
+            predicted_emotion = emotions[max_index]
+
+            cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+            textEmotion = predicted_emotion    
+        resized_img = cv2.resize(test_img, (width, height))
+        cv2.imshow('Facial emotion analysis ', resized_img)
+        out.write(resized_img)
+    else:
+        print('Does not detect this frame ' + str(currentFrame))
+    
     print('--------------------------------')
+    print('Frame number: ' + str(currentFrame))
+    print('Total frame: ' + str(cap.get(cv2.CAP_PROP_FRAME_COUNT)))
+    print('FPS : ' + str(cap.get(cv2.CAP_PROP_FPS)))
     print(str(i) + ': ' + textEmotion)
     print(*result, sep = ", ")
     print('--------------------------------')
     i += 1
+    print('Total detected frames: ' + str(count))
 
-
-    if cv2.waitKey(10) == ord('q'):#wait until 'q' key is pressed
+    if(currentFrame == cap.get(cv2.CAP_PROP_FRAME_COUNT)):
         break
 
 cap.release()
+out.release()
 cv2.destroyAllWindows
